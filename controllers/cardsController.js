36a8575c5eb7e3c18a5bc2bfb92297422ad6ea21
NicolasPastorio <npastoriodemoura@gmail.com>
2024-11-08 16:21:39 -0300

    Concluído a renderização dos cardes e das categorias na home

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -1,10 +1,19 @@
 const Imovel = require('../models/Imovel');
 
 exports.renderHome = (req, res) => {
-    Imovel.getApartamentos((err, apartamentos) => {
+    /*Imovel.getApartamentos((err, apartamentos) => {
         if(err){
             return res.status(500).send('Erro ao buscar apartamentos!');
         }
         res.render('home', { apartamentos });
-    });
+    });*/
+
+    Imovel.getCategoriasComImoveis()
+        .then((categoriasComImoveis) => {
+            res.render('home', { categoriasComImoveis });
+        })
+        .catch((err) => {
+            console.error('Erro ao buscar dados: ', err);
+            res.status(500).send('Erro ao carregar imóveis.');
+        });
 };
--- models/Imovel.js
+++ models/Imovel.js
@@ -1,7 +1,7 @@
 const db = require('../config/database');
 
 class Imovel {
-    static getApartamentos(callback){
+    /*static getApartamentos(callback){
         const query = `
             SELECT Imovel.Descricao
             FROM Imovel
@@ -17,7 +17,34 @@ class Imovel {
                 callback(null, results);
             }
         });
+    }*/
+    static getCategoriasComImoveis(){
+        return new Promise((resolve, reject) => {
+            //Busca todas as categorias
+            const categoriasQuery = 'SELECT * FROM Categorias;';
+
+            db.query(categoriasQuery, (err, categorias) => {
+                if(err) return reject(err);
+
+                //Para cada categoria, busca os imóveis relacionados
+                const promises = categorias.map(categoria => {
+                    const imoveisQuery = `SELECT * FROM Imovel WHERE idCategorias = ?;`;
+
+                    return new Promise((resolve, reject) => {
+                        db.query(imoveisQuery, [categoria.idCategorias], (err, imoveis) => {
+                            if(err) return reject(err);
+                            resolve({
+                                categoria,
+                                imoveis
+                            });
+                        });
+                    });
+                });
+                Promise.all(promises).then(results => resolve(results)).catch(err => reject(err));
+            });
+        });
     }
+    
 }
 
 module.exports = Imovel;
